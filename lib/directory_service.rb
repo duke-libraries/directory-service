f78efdff6091202546ad9f4b2dc6a9e77fe2bc7c
dchandekstark <dchandekstark@gmail.com>
2014-05-23 10:09:59 -0400

    Changed to use instance methods rather than class methods Added filters

--- lib/directory_service.rb
+++ lib/directory_service.rb
@@ -2,10 +2,21 @@ require 'net-ldap'
 
 class DirectoryService
 
-  class_attribute :host, :base, :scope, :attributes
-  self.scope = Net::LDAP::SearchScope_SingleLevel
+  attr_accessor :host, :base, :scope, :attributes
+  
+  DEFAULT_SCOPE = Net::LDAP::SearchScope_SingleLevel
+  NETID_ATTRIBUTE = "uid"
+  DUID_ATTRIBUTE = "dudukeid"
+  NAME_ATTRIBUTE = "cn"
 
-  def self.search_results(query)
+  def initialize(config={})
+    @host = config.fetch :host
+    @base = config.fetch :base
+    @scope = config.fetch :scope, DEFAULT_SCOPE
+    @attributes = config[:attributes] # nil or empty array retrieves all attributes
+  end
+
+  def search_results(query)
     client.search(search_args(query)).each_with_object([]) do |result, memo|
       memo << result.attribute_names.each_with_object({}) { |attr, h| h[attr] = result[attr].first }
     end
@@ -15,18 +26,30 @@ class DirectoryService
     yield self
   end
 
-  def self.search_args(query)
-    {filter: filter(query), attributes: attributes}
+  private
+
+  def search_args(query)
+    {filter: combo_filter(query), attributes: attributes}
   end
 
-  def self.filter(query)
-    Net::LDAP::Filter.contains("cn", query)
+  def combo_filter(query)
+    duid_filter(query) | netid_filter(query) | name_filter(query)
   end
 
-  def self.client
+  def name_filter(query)
+    Net::LDAP::Filter.contains(NAME_ATTRIBUTE, query)
+  end
+
+  def netid_filter(query)
+    Net::LDAP::Filter.eq(NETID_ATTRIBUTE, query)
+  end
+
+  def duid_filter(query)
+    Net::LDAP::Filter.eq(DUID_ATTRIBUTE, query)
+  end
+
+  def client
     Net::LDAP.new(host: host, base: base, scope: scope)
   end
-
-  private_class_method :new, :search_args, :filter, :client
 
 end
