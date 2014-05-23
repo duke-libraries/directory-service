4adfd04e4b9c8e7af042608d306211ae6e242f0e
dchandekstark <dchandekstark@gmail.com>
2014-05-23 12:34:01 -0400

    Added DukeDirectoryService subclass Added Gemfile

--- lib/directory_service.rb
+++ lib/directory_service.rb
@@ -5,47 +5,31 @@ class DirectoryService
   attr_accessor :host, :base, :scope, :attributes
   
   DEFAULT_SCOPE = Net::LDAP::SearchScope_SingleLevel
-  NETID_ATTRIBUTE = "uid"
-  DUID_ATTRIBUTE = "dudukeid"
-  NAME_ATTRIBUTE = "cn"
 
   def initialize(config={})
-    @host = config.fetch :host
-    @base = config.fetch :base
+    @host = config.fetch :host, ENV['DIRECTORY_HOST']
+    @base = config.fetch :base, ENV['DIRECTORY_BASE']
     @scope = config.fetch :scope, DEFAULT_SCOPE
     @attributes = config[:attributes] # nil or empty array retrieves all attributes
   end
 
-  def search_results(query)
-    client.search(search_args(query)).each_with_object([]) do |result, memo|
-      memo << result.attribute_names.each_with_object({}) { |attr, h| h[attr] = result[attr].first }
+  def search_results(filter)
+    client.search(filter: filter, attribute: attributes)
+  end
+
+  # Returns search results normalized as hash with string values
+  def search_results_s(filter)
+    search_results(filter).collect do |entry|
+      entry.attribute_names.each_with_object({}) { |attr, memo| memo[attr] = entry[attr].first }
     end
   end
 
-  def self.configure
-    yield self
-  end
-
-  private
-
-  def search_args(query)
-    {filter: combo_filter(query), attributes: attributes}
-  end
-
-  def combo_filter(query)
-    duid_filter(query) | netid_filter(query) | name_filter(query)
-  end
-
   def name_filter(query)
-    Net::LDAP::Filter.contains(NAME_ATTRIBUTE, query)
+    Net::LDAP::Filter.contains("cn", query)
   end
 
-  def netid_filter(query)
-    Net::LDAP::Filter.eq(NETID_ATTRIBUTE, query)
-  end
-
-  def duid_filter(query)
-    Net::LDAP::Filter.eq(DUID_ATTRIBUTE, query)
+  def uid_filter(query)
+    Net::LDAP::Filter.eq("uid", query)
   end
 
   def client
---
+++ lib/duke_directory_service.rb
@@ -0,0 +1,16 @@
+class DukeDirectoryService < DirectoryService
+
+  def netid_or_duid_search(query)
+    filter = duid_filter(query) | netid_filter(query)
+    search_results_s filter
+  end
+
+  def netid_filter(query)
+    uid_filter(query)
+  end
+
+  def duid_filter(query)
+    Net::LDAP::Filter.eq("dudukeid", query)
+  end
+
+end
